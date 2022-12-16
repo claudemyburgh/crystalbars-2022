c3a97daca07bfe601c29c045b714855e25626ebf
claudemyburgh <claudemyburgh@gmail.com>
2022-12-16 20:14:12 +0200

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,55 +2,6 @@
 import "vanilla-headless"
 import SimpleLightboxEsm from "simplelightbox/dist/simple-lightbox.esm"
 import "simplelightbox/dist/simple-lightbox.css"
-import barba from "@barba/core"
-// import { gsap } from "gsap";
-// import barbaCss from '@barba/css';
-// import barbaRouter from "@barba/router"
-//
-// const myRoutes = [
-//   {
-//     path: "/",
-//     name: "home",
-//   },
-//   {
-//     path: "/faq",
-//     name: "faq",
-//   },
-//   {
-//     path: "/products",
-//     name: "products",
-//   },
-//   {
-//     path: "/gallery",
-//     name: "gallery",
-//   },
-//   {
-//     path: "/quote",
-//     name: "quote",
-//   },
-// ]
-//
-// barba.use(barbaRouter, {
-//   routes: myRoutes
-// });
-
-// barba.init({
-    // transitions: [{
-    //   name: 'opacity-transition',
-    //   leave(data) {
-    //     return gsap.to(data.current.container, {
-    //       opacity: 0,
-    //       duration: 0.15
-    //     });
-    //   },
-    //   enter(data) {
-    //     return gsap.from(data.next.container, {
-    //       opacity: 0,
-    //       duration: 0.15
-    //     });
-    //   }
-    // }]
-// });
 
 function init() {
   new SimpleLightboxEsm(".gallery a", {
